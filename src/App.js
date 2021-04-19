580f053ee0c9dfdbc6d59b5df933fc9fde893e6d
Michael <Mlcorrieri@gmail.com>
2021-04-19 17:12:19 -0400

    Need to fix link router for single lake display page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Home from "./Home";
 import Header from "./Header";
 import Login from "./Login";
+import LakeCard from "./LakeCard";
 
 function App() {
   const [lakesArr, setLakesArr] = useState([]);
@@ -19,16 +21,31 @@ function App() {
     setLakesArr(updatedLakeArr);
   }
 
-  function handleDeleteLake(deletedLake){
-    const newLakeArr = lakesArr.filter((lake) => lake !== deletedLake)
-    setLakesArr(newLakeArr)
+  function handleDeleteLake(deletedLake) {
+    const newLakeArr = lakesArr.filter((lake) => lake !== deletedLake);
+    setLakesArr(newLakeArr);
   }
 
   return (
     <div>
       <Header />
-      <Home lakesArr={lakesArr} onAddNewLake={handleAddLake} onDeleteLake={handleDeleteLake}/>
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home
+              lakesArr={lakesArr}
+              onAddNewLake={handleAddLake}
+              onDeleteLake={handleDeleteLake}
+            />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/lakes/:id">
+            <LakeCard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/LakeCard.js
+++ src/LakeCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function LakeCard({ lake, onDeleteLake }) {
   const { id, name, image, location, description, wantToVisit, link } = lake;
@@ -15,10 +16,11 @@ function LakeCard({ lake, onDeleteLake }) {
     <div>
       <h2>{name}</h2>
       <img src={image} alt={name} />
+      {/* <Link to={`lakes/${id}`}>hi there</Link> */}
       <h3>{location}</h3>
       <p>{description}</p>
       <button onClick={() => setHaveVisited(!haveVisited)}>
-        {haveVisited ? "Down to Visit 😏" : "No thanks🙄"}
+        {haveVisited ? "Down to Visit 😏" : "No thanks 🙄"}
       </button>
       <button onClick={handleDeleteLake}>🗑️</button>
       <a href={link}>More Info</a>
